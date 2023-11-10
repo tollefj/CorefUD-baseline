a986e9ec11e7afb1a3936301efaf129590fa5f75
Tollef Jørgensen <tollefj@gmail.com>
2023-11-10 15:29:26 +0100

    update datafetcher

--- data/get.sh
+++ data/get.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 
-# This file is part of CorPipe <https://github.com/ufal/crac2022-corpipe>.
-#
+# This file is based on CorPipe <https://github.com/ufal/crac2022-corpipe>.
 # Copyright 2022 Institute of Formal and Applied Linguistics, Faculty of
 # Mathematics and Physics, Charles University in Prague, Czech Republic.
-#
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-# Train and dev
 curl --remote-name-all https://lindat.mff.cuni.cz/repository/xmlui/bitstream/handle/11234/1-5053{/CorefUD-1.1-public.zip}
 unzip CorefUD-1.1-public.zip
 for f in CorefUD-1.1-public/data/*/*.conllu; do
@@ -22,7 +19,6 @@ rm -r CorefUD-1.1-public/ CorefUD-1.1-public.zip
 
 mkdir dev
 (cd dev
-# wget https://ufal.mff.cuni.cz/~mnovak/files/corefud-1.1/dev-blind.zip
 curl --remote-name-all https://ufal.mff.cuni.cz/~mnovak/files/corefud-1.1{/dev-blind.zip}
 unzip dev-blind.zip
 for f in *.conllu; do
@@ -32,10 +28,8 @@ done
 )
 rm -r dev/
 
-# # Test
 mkdir test
   (cd test
-  # wget https://ufal.mff.cuni.cz/~mnovak/files/corefud-1.1/test-blind.zip
   curl --remote-name-all https://ufal.mff.cuni.cz/~mnovak/files/corefud-1.1{/test-blind.zip}
   unzip test-blind.zip
   for f in *.conllu; do
@@ -44,6 +38,3 @@ mkdir test
   done
   )
 rm -r test/
-
-# # Data cleanup
-# sed 's/20.1	trabajó	trabajar/20.1	_	_/' -i es_ancora/es_ancora-corefud-train.conllu
